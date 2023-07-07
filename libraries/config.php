6190a547606b6995a2fc0e410e276aa7802f45aa
David Radford <dave@bigredradish.com>
2023-07-07 13:42:05 +0100

    Create config.php

---
+++ libraries/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * may do this differently
+ */
+
+// Define some constants
+  define("DB_HOST", " ");
+  define("DB_NAME", " ");
+  define("DB_USER", " ");
+  define("DB_PASS", " ");
+  
+  define("VERSION", "0.0.1");
